6206564c4332e5a3d7f81a6a91b9e0feb7f69226
Nicolas <nakasonenicolas13@gmail.com>
2022-10-10 22:29:40 -0300

    Se cambio color buscador.js

--- nuevoproyect/src/Components/Buscador.js
+++ nuevoproyect/src/Components/Buscador.js
@@ -4,12 +4,14 @@ import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 import "../Stylesheets-Components/Buscador.css"
 
+
+//Modficar estilos desde aca:
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    backgroundColor: alpha(theme.palette.common.white, 1.15),
     '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
+      backgroundColor: alpha(theme.palette.common.white, 0.75),
     },
     marginLeft: 0,
     width: '100%',
